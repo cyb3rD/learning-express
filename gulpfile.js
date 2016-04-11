18df159ec1a07efb5f0a82efd0618b6ac640c9e9
Nikolay Ignatyev <noilgt@gmail.com>
2016-04-12 00:16:26 +0300

    Added bower js&css injections

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var wiredep = require('wiredep').stream;
 
 var jsFiles = ['*.js', 'src/**/*.js'];
+var htmlFiles = ['src/views/*.html'];
 
 gulp.task('style', function() {
-    gulp.src(jsFiles)
+    return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
             verbose: true
         }))
         .pipe(jscs())
         .pipe(jscs.reporter());
+});
+
+gulp.task('inject', function() {
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+    return gulp.src(htmlFiles)
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
 });
